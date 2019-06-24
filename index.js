81af19e198b15b57568715d28eb20b6c9dc9bfe5
NielsAudoor <nielsje3@gmail.com>
2019-06-25 00:26:03 +0200

    command prediction

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const readdir = require('fs').readdir;
+var stringSimilarity = require('string-similarity');
 
 config = require('./config.json')
 bot.prefix = '!'
@@ -29,19 +30,38 @@ bot.on('ready', () => {
 });
 
 bot.on('message', message =>{
+    if (message.author.bot) {
+        return;
+    }
+    
     message.args = message.content.split(/\s+/g);
     message.content = message.content.substring(message.content.indexOf(' ') + 1, message.content.length) || null;
     let command = message.args.shift().slice(bot.prefix.length).toLowerCase();
+    if (!message.content.startsWith(bot.prefix)) {
+        return;
+    }
     let cmd;
+    let predictionPercent = 0;
+    let predictionCommand;
+    let predictionCommandName;
     
     for(i = 0; i< bot.commands.array().length; i++) {
         for(j=0; j<bot.commands.array()[i].name.length; j++){
-            if(bot.commands.array()[i].name[j] === command){
-                console.log("command found")
-                cmd = bot.commands.array()[i];
+            let predictionScore = stringSimilarity.compareTwoStrings(command , bot.commands.array()[i].name[j])*100
+            if(predictionScore > predictionPercent){
+                predictionPercent = predictionScore
+                predictionCommand = bot.commands.array()[i]
+                predictionCommandName = bot.commands.array()[i].name[j]
+                console.log(bot.commands.array()[i].name[j]+" - "+predictionScore+"%")
             }
         }
     }
+    if(predictionPercent < 75){
+        message.channel.send(`I could not find that command, did you mean !${predictionCommandName}? (${predictionPercent.toString().substring(0, 2)}% match)`)
+        return;
+    } else {
+        cmd = predictionCommand;
+    }    
 
     if(command){
         console.log(message.content);
