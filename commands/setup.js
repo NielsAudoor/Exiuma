e72aaa9d5805ae2e734ba0737e440273660926a5
NielsAudoor <nielsje3@gmail.com>
2019-06-26 14:31:59 +0200

    mongoDB

--- commands/setup.js
+++ commands/setup.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: ['async', 'start', 'setup'],
-    main: async function(bot, message) {
+    main: async function (bot, message) {
         const Discord = require('discord.js');
         var stringSimilarity = require('string-similarity');
         let enableLogChannel;
@@ -9,6 +9,16 @@ module.exports = {
         let enableDadMode;
         let prediction;
         let predictionPercent = 0;
+
+        var mongoUtil = require('../mongoUtil');
+        var db = mongoUtil.getDb();
+
+        var myobj = { name: "Company Inc", address: "Highway 37" };
+        db.collection('logging').insertOne(myobj, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+        })
+
         let yes = [
             'yes', 'yeah', 'yup', 'sure', 'ok', 'yep',
         ]
@@ -48,9 +58,14 @@ module.exports = {
         async function promptUser(msg) {
             return new Promise(result => {
                 message.channel.send(msg)
-                message.channel.awaitMessages(filter, {max: 1, time: 10000}).then(collected => {
-                    if (collected.first().content) {
-                        result(collected.first().content)
+                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                    if (collected.first().attachments.size == 0) {
+                        if (collected.first().content) {
+                            result(collected.first().content)
+                        }
+                    }
+                    else {
+                        message.channel.send("Please don't send a picture during setup.");
                     }
                 })
             });
@@ -66,7 +81,7 @@ module.exports = {
 
         async function testSetup() {
             var result = await ask("This is a test yes/no function?");
-            if(result) {
+            if (result) {
                 console.log("yes")
             } else {
                 console.log("no")
@@ -74,7 +89,7 @@ module.exports = {
         }
         async function logChannelSetup() {
             var result = await ask("Do you want a log channel?");
-            if(result) {    //if yes
+            if (result) {    //if yes
                 message.channel.send('Ok, I will go ahead and add a log channel!')
                 enableLogChannel = "Enabled";
                 message.guild.createChannel('Logs', {
@@ -105,7 +120,7 @@ module.exports = {
                     }]
                 })
                 var result2 = await ask("Great! do you want a custom greeting?");
-                if(result2) {   //if yes
+                if (result2) {   //if yes
                     var result3 = await promptUser('Fantastic! What do you want your custom greeting to be?');
                     message.channel.send(`Great I will set your custom greeting to "${result3}"!`)
                     customGreeting = result3;
@@ -124,32 +139,34 @@ module.exports = {
         }
         async function DadModeSetup() {
             var result = await ask("One last thing - Do you want me to make dad jokes?");
-            if(result) {
+            if (result) {
                 message.channel.send('Ok, I will be sure to make plenty of dad jokes ;)')
                 enableDadMode = "Enabled";
-                ReviewSetup()            } else {
+                ReviewSetup()
+            } else {
                 message.channel.send('Ok, I wont wont make dad jokes ;)')
                 enableDadMode = "Disabled";
-                ReviewSetup()            }
+                ReviewSetup()
+            }
         }
         async function ReviewSetup() {
             message.channel.send("Aaaand we are done! Here are the settings for your server:")
             var embed = new Discord.RichEmbed()
                 .setTitle("Server Settings!")
-                .addField('Log channel', "```"+enableLogChannel+"```")
-                .addField('Welcome channel', "```"+enableWelcomeChannel+"```")
-                .addField('Greeting', "```"+customGreeting+"```")
-                .addField('Dad mode', "```"+enableDadMode+"```")
+                .addField('Log channel', "```" + enableLogChannel + "```")
+                .addField('Welcome channel', "```" + enableWelcomeChannel + "```")
+                .addField('Greeting', "```" + customGreeting + "```")
+                .addField('Dad mode', "```" + enableDadMode + "```")
                 .setThumbnail(message.guild.iconURL)
-                .setColor([255,255,255]);
+                .setColor([255, 255, 255]);
             message.channel.send(embed)
-            setTimeout(function() {
+            setTimeout(function () {
                 FinalizeSetup()
-            },1000)
+            }, 1000)
         }
         async function FinalizeSetup() {
             var result = await ask("Do these settings look ok to you?");
-            if(result) {
+            if (result) {
                 message.channel.send('Great because I cant change them!')
                 message.channel.send('Goodbye!')
             } else {
--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const readdir = require('fs').readdir;
 var stringSimilarity = require('string-similarity');
 config = require('./config.json')
 
+var mongoUtil = require('./mongoUtil');
+
+mongoUtil.connectToServer(function (err, client) {
+    if (err) console.log(err);
+});
+
 bot.devs = [
     '247015447885512714', '200949063061864448',
 ];
@@ -19,15 +25,15 @@ bot.sleep = function (milliseconds) {
     }
 };
 async function reactionCatcher(msg, predictionCommand, unknownCommandFlag, predictionCommandName, filter, message) {
-    setTimeout(function() {
+    setTimeout(function () {
         msg.clearReactions();
     }, 10000)
-    msg.awaitReactions(filter, {max: 1, time: 10000}).then(collected => {
-        if(collected){
-            if(collected.first().emoji.name === "✅"){
+    msg.awaitReactions(filter, { max: 1, time: 10000 }).then(collected => {
+        if (collected) {
+            if (collected.first().emoji.name === "✅") {
                 msg.clearReactions()
                 msg.edit(`Got it! Running !${predictionCommandName}...`);
-                if(unknownCommandFlag){
+                if (unknownCommandFlag) {
                     try {
                         predictionCommand.main(bot, message);
                         unknownCommandFlag = 0;
@@ -62,14 +68,14 @@ bot.on('ready', () => {
 });
 
 //message listener
-bot.on('message', message =>{
+bot.on('message', message => {
     message.args = message.content.split(/\s+/g);
     message.content = message.content.substring(message.content.indexOf(' ') + 1, message.content.length) || null;
     let command = message.args[0].slice(bot.prefix.length).toLowerCase()
     if (!message.args[0].startsWith(bot.prefix)) {
         return;
     }
-    if(message.channel.type == 'dm'){
+    if (message.channel.type == 'dm') {
         return;
     }
     let cmd;
@@ -89,45 +95,45 @@ bot.on('message', message =>{
     }
 
     //prediction engine
-    for(i = 0; i< bot.commands.array().length; i++) {
-        for(j=0; j<bot.commands.array()[i].name.length; j++){
-            let predictionScore = stringSimilarity.compareTwoStrings(command , bot.commands.array()[i].name[j])*100
-            if(predictionScore > predictionPercent){
+    for (i = 0; i < bot.commands.array().length; i++) {
+        for (j = 0; j < bot.commands.array()[i].name.length; j++) {
+            let predictionScore = stringSimilarity.compareTwoStrings(command, bot.commands.array()[i].name[j]) * 100
+            if (predictionScore > predictionPercent) {
                 predictionPercent = predictionScore
                 predictionCommand = bot.commands.array()[i]
                 predictionCommandName = bot.commands.array()[i].name[j]
-                console.log(bot.commands.array()[i].name[j]+" - "+predictionScore+"%")
+                console.log(bot.commands.array()[i].name[j] + " - " + predictionScore + "%")
             }
         }
     }
     let roundedPredictionPercent = predictionPercent
-    if (roundedPredictionPercent === 100){
+    if (roundedPredictionPercent === 100) {
         roundedPredictionPercent = predictionPercent.toString().substring(0, 3)
     } else {
         roundedPredictionPercent = predictionPercent.toString().substring(0, 2)
     }
-    if(predictionPercent < 75 && predictionPercent > 0){
+    if (predictionPercent < 75 && predictionPercent > 0) {
         unknownCommandFlag = 1;
         message.channel.send(`I could not find that command, did you mean !${predictionCommandName}? (${roundedPredictionPercent}% match)`).then(msg => {
             msg.react("✅");
             const filter = (reaction, user) => user.id === message.author.id && reaction.emoji.name === "✅";
             reactionCatcher(msg, predictionCommand, unknownCommandFlag, predictionCommandName, filter, message);
         })
-    } else if (predictionPercent > 75){
+    } else if (predictionPercent > 75) {
         unknownCommandFlag = 0;
         cmd = predictionCommand;
-    } else if (predictionPercent === 0){
+    } else if (predictionPercent === 0) {
         message.channel.send("I could not find that command")
         return;
     }
 
-    if(command){
-        console.log(message.author.username+` (${message.guild.name}) - `+message.content+` (${roundedPredictionPercent}%)`);
+    if (command) {
+        console.log(message.author.username + ` (${message.guild.name}) - ` + message.content + ` (${roundedPredictionPercent}%)`);
     }
 
     //actually running the command
     try {
-        if(cmd && !unknownCommandFlag) {
+        if (cmd && !unknownCommandFlag) {
             cmd.main(bot, message);
         }
     } catch (err2) {
