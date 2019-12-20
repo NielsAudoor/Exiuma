b2e888494e3c4095c572680a2ef68577479f23e1
Ryan <ryan@thebacons.us>
2019-12-19 18:16:13 -0700

    some really small changes

--- processes/dynamicVc.js
+++ processes/dynamicVc.js
@@ -331,7 +331,7 @@ module.exports = {
                         if (channel.members.array()[i].presence.game) {
                             if (!desc) {
                                 if (!channel.members.array()[i].user.equals(bot.user)) {
-                                    if(channel.members.array()[i].presence.game.name !== "Spotify"){
+                                    if(channel.members.array()[i].presence.game.name !== "Spotify" && channel.members.array()[i].presence.game.name !== "Custom Status") {
                                         if(!channel.members.array()[i].presence.game.streaming) {
                                             desc = channel.members.array()[i].presence.game.name
                                             games.push(channel.members.array()[i].presence.game.name);
@@ -341,7 +341,7 @@ module.exports = {
                             } else {
                                 if(games.indexOf(channel.members.array()[i].presence.game.name) < 0){
                                     if (!channel.members.array()[i].user.equals(bot.user)) {
-                                        if(channel.members.array()[i].presence.game.name !== "Spotify") {
+                                        if(channel.members.array()[i].presence.game.name !== "Spotify" && channel.members.array()[i].presence.game.name !== "Custom Status") {
                                             if(!channel.members.array()[i].presence.game.streaming){
                                                 desc += ", " + channel.members.array()[i].presence.game.name
                                                 games.push(channel.members.array()[i].presence.game.name);
